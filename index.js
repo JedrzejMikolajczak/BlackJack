dd256478e2fac56a176ec13fcd8d2a6f93d93468
JedrzejMikolajczak <jedrzej.mikolajczak@gmail.com>
2025-09-30 20:23:43 +0100

    das

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// 2. Create the player object. Give it two keys, name and chips, and set their values
+
 let player = {
     name: "Per",
     chips: 200
@@ -11,10 +11,10 @@ let message = ""
 let messageEl = document.getElementById("message-el")
 let sumEl = document.getElementById("sum-el")
 let cardsEl = document.getElementById("cards-el")
-// 3. Grab ahold of the player-el paragraph and store it in a variable called playerEl
+
 let playerEl = document.getElementById("player-el")
 
-// 4. Render the player's name and chips in playerEl
+
 playerEl.textContent = player.name + ": $"
 
 function getRandomCard() {
